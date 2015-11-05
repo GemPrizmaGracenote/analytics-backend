457236dffb2b25e110b89910e0bf16627cc4e4a1
Ted Kaplan <ted@fem-inc.com>
2015-11-04 19:04:41 -0800

    Add event handler

---
+++ handle_event.js
@@ -0,0 +1,45 @@
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+AWS.config.update({region: 'us-east-1'});
+var config = JSON.parse(fs.readFileSync('config.json'));
+var KEY_DELIM = '|';
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var DEFAULT_PERIOD = {
+  name: 'hourly',
+  modulus: 60*60*1000 // ms
+};
+
+exports.handler = function(event, context) {
+  var timestamp = new Date().getTime();
+  var bucket = timestamp - (timestamp % DEFAULT_PERIOD.modulus);
+  config.breakdowns.forEach(function(breakdown) {
+    var key = breakdown.dimensions.map(function(dimension) {
+      return event[dimension];
+    }).join(KEY_DELIM);
+
+    docClient.update({
+      TableName: breakdown.name + '_' + DEFAULT_PERIOD.name, // TODO(ted): Make this configurable
+      Key: {
+        key: key,
+        timestamp: bucket
+      },
+      UpdateExpression: 'SET #event_name = if_not_exists(#event_name, :zero) + :one',
+      ExpressionAttributeNames: {
+        '#event_name': event.event_name + '_count',
+      },
+      ExpressionAttributeValues: {
+        ':zero': 0,
+        ':one': 1
+      }
+    }, function(error) {
+      if (error) {
+        console.log(error.message);
+        context.fail(error.message);
+      } else {
+        context.succeed();
+      }
+    });
+  });
+}
