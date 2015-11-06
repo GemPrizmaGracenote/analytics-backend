2bf6b7f0b3356747183883a0046a904c616522ae
Ted Kaplan <ted@fem-inc.com>
2015-11-05 18:11:04 -0800

    Add support for extra_aggregations

--- handle_event.js
+++ handle_event.js
@@ -1,6 +1,7 @@
 var AWS = require('aws-sdk');
 var fs = require('fs');
 var Promise = require('bluebird');
+var _ = require('lodash');
 
 AWS.config.update({region: 'us-east-1'});
 var config = JSON.parse(fs.readFileSync('config.json'));
@@ -13,6 +14,30 @@ var DEFAULT_PERIOD = {
   modulus: 60*60*1000 // ms
 };
 
+function incrementExpression(name, incr) {
+  return name + ' = if_not_exists(' + name + ', :zero) + ' + incr;
+}
+
+function buildExtraAggregationParams(aggregations, event) {
+  var expressionAttributeNames = {};
+  var expressionAttributeValues = {};
+  var updateExpression = [''];
+  aggregations.forEach(function(agg, i) {
+    var value = _.get(event, agg.property);
+    var sym = 'a' + i;
+    var attrName = '#' + sym;
+    var attrValue = ':' + sym;
+    expressionAttributeNames[attrName] = event.event_name + '_' + agg.name;
+    expressionAttributeValues[attrValue] = value;
+    updateExpression.push(incrementExpression(attrName, attrValue));
+  });
+  return {
+    ExpressionAttributeNames: expressionAttributeNames,
+    ExpressionAttributeValues: expressionAttributeValues,
+    UpdateExpression: updateExpression.join(', ')
+  };
+}
+
 exports.handler = function(event, context) {
   var timestamp = new Date().getTime();
   var bucket = timestamp - (timestamp % DEFAULT_PERIOD.modulus);
@@ -20,6 +45,7 @@ exports.handler = function(event, context) {
     var key = breakdown.dimensions.map(function(dimension) {
       return event[dimension];
     }).join(KEY_DELIM);
+    var aggregationParams = buildExtraAggregationParams(config.extra_aggregations, event);
 
     return docClient.updateAsync({
       TableName: breakdown.name + '_' + DEFAULT_PERIOD.name, // TODO(ted): Make this configurable
@@ -27,14 +53,15 @@ exports.handler = function(event, context) {
         key: key,
         timestamp: bucket
       },
-      UpdateExpression: 'SET #event_name = if_not_exists(#event_name, :zero) + :one',
-      ExpressionAttributeNames: {
-        '#event_name': event.event_name + '_count',
-      },
-      ExpressionAttributeValues: {
+      UpdateExpression: 'SET ' + incrementExpression('#event_count', ':one') +
+          aggregationParams.UpdateExpression,
+      ExpressionAttributeNames: _.assign({
+        '#event_count': event.event_name + '_count',
+      }, aggregationParams.ExpressionAttributeNames),
+      ExpressionAttributeValues: _.assign({
         ':zero': 0,
         ':one': 1
-      }
+      }, aggregationParams.ExpressionAttributeValues)
     })
   })).then(context.succeed.bind(context), function(error) {
     console.log(error.message);
